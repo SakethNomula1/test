637c6391e609cec624bbd971b1356c69b79cea57
saketh nomula1 <nomulasaketh95@gmail.com>
2024-01-24 09:57:21 +0530

    my file

---
+++ alpha.java
@@ -0,0 +1,40 @@
+/*class alpha
+{
+public static void main(String args[])
+{
+ char name='A';
+ if(name>='A' && name<='Z')
+  {
+   System.out.println("capital");
+  }
+ else 
+     {
+      System.out.println("small letter");
+     }
+}
+}*/
+
+class vowel
+{
+public static void main(String args[])
+{
+ char ch=4;
+ if(ch=='a'&& ch=='e'&& ch=='i' && ch=='o' && ch=='u')
+ {
+  System.out.println("the given alphabate is vowel");
+ }
+ 
+ else if(ch>=0 && ch<=10)
+ {
+  System.out.println("the given input is a number");
+ }
+ else
+   {
+    System.out.println("the given alphabate is a consonant");
+   }
+  }
+}
+
+  
+
+ 
